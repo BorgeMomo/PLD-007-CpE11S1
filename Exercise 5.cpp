a6b841c44f3c298c5b1e25f026814ec4bb0510be
BorgeMomo <qbrmomo@tip.edu.ph>
2024-09-25 12:16:05 +0800

    Add files via upload

---
+++ Exercise 5.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	//Create a C++ to determine if alphabet is a vowel or a consonant
+	char c;
+	
+	cout<<"Enter a letter: ";
+	cin>>c;
+	
+	switch(c)
+	{
+		case 'a':
+		cout<<"Vowel!"<<endl;
+		break;
+		
+		case 'A':
+		cout<<"Vowel!"<<endl;
+		break;
+		
+		case 'e':
+		cout<<"Vowel!"<<endl;
+		break;
+		
+		case 'E':
+		cout<<"Vowel!"<<endl;
+		break;
+		
+		case 'i':
+		cout<<"Vowel!"<<endl;
+		break;
+		
+		case 'I':
+		cout<<"Vowel!"<<endl;
+		break;
+		
+		case 'o':
+		cout<<"Vowel!"<<endl;
+		break;
+		
+		case 'O':
+		cout<<"Vowel!"<<endl;
+		break;
+		
+		case 'u':
+		cout<<"Vowel!"<<endl;
+		break;
+
+		case 'U':
+		cout<<"Vowel!"<<endl;
+		break;
+		
+		default:
+			{
+			cout<<"Consonant!"<<endl;
+			break;
+			}
+	}
+
+	return 0;
+}
