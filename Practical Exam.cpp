47d8e688d66b3240964b24103a71e456773d098f
BorgeMomo <qbrmomo@tip.edu.ph>
2024-10-02 12:15:12 +0800

    Add files via upload

---
+++ Practical Exam.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//Practical Exam
+int main(){
+	float x = 0;
+	float y = 0;
+	//Enter the numbers:
+	cout<<"Enter First number: ";
+	cin>>x;
+	cout<<"Enter Second number: ";
+	cin>>y;
+	//The answers:
+	cout<<"The sum is "<<x+y<<endl;
+	cout<<"The average is "<<(x+y)/2<<endl;
+	cout<<"The sum of the squares of the following numbers are: "<<(x*x)+(y*y)<<endl;
+	return 0;
+}
